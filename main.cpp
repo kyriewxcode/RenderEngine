1c3a642f7e078e5657faaed32d5ebf1fc0be7937
Kyrie <61609701+kyriewxcode@users.noreply.github.com>
2021-11-15 18:09:59 +0800

    遮挡剔除

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 ﻿#include <vector>
 #include <cmath>
+#include <cstdlib>
+#include <limits>
 #include "tgaimage.h"
 #include "model.h"
 #include "geometry.h"
@@ -41,68 +43,86 @@ void line(Vec2i p0, Vec2i p1, TGAImage& image, TGAColor color)
 	}
 }
 
-void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage& image, TGAColor color)
+Vec3f barycentric(Vec3f A, Vec3f B, Vec3f C, Vec3f P)
 {
-	if (t0.y == t1.y && t0.y == t2.y) return; // 不关心退化三角形
-	if (t0.y > t1.y) std::swap(t0, t1);
-	if (t0.y > t2.y) std::swap(t0, t2);
-	if (t1.y > t2.y) std::swap(t1, t2);
-
-	int total_height = t2.y - t0.y;
-	for (int i = 0; i < total_height; i++)
+	Vec3f s[2];
+	for (int i = 2; i--;)
 	{
-		bool second_half = i > t1.y - t0.y || t1.y == t0.y;	// 判断是否上半三角形
-		int segment_height = second_half ? t2.y - t1.y : t1.y - t0.y;
-		float alpha = (float)i / total_height;
-		float beta = (float)(i - (second_half ? t1.y - t0.y : 0)) / segment_height; // 不可以除零
-		Vec2i A = t0 + (t2 - t0) * alpha;
-		Vec2i B = second_half ? t1 + (t2 - t1) * beta : t0 + (t1 - t0) * beta;
-		if (A.x > B.x) std::swap(A, B);
-		for (int j = A.x; j < B.x; j++)
+		s[i][0] = C[i] - A[i];
+		s[i][1] = B[i] - A[i];
+		s[i][2] = A[i] - P[i];
+	}
+	Vec3f u = cross(s[0], s[1]);
+	if (std::abs(u[2]) > 1e-2)
+		return Vec3f(1.f - (u.x + u.y) / u.z, u.y / u.z, u.x / u.z);
+	return Vec3f(-1, 1, 1); // 负坐标将会被忽略
+}
+
+void triangle(Vec3f* pts, float* zbuffer, TGAImage& image, TGAColor color)
+{
+	Vec2f bboxmax(-std::numeric_limits<float>::max(), -std::numeric_limits<float>::max());
+	Vec2f bboxmin(std::numeric_limits<float>::max(), std::numeric_limits<float>::max());
+	Vec2f clamp(image.get_width() - 1, image.get_height() - 1);
+
+	for (int i = 0; i < 3; ++i)
+	{
+		for (int j = 0; j < 2; ++j)
 		{
-			image.set(j, t0.y + i, color); // t0.y+i != A.y
+			bboxmax[j] = std::min(clamp[j], std::max(bboxmax[j], pts[i][j]));
+			bboxmin[j] = std::max(0.0f, std::min(bboxmin[j], pts[i][j]));
 		}
 	}
+	Vec3f P;
+	for (P.x = bboxmin.x; P.x <= bboxmax.x; P.x++)
+	{
+		for (P.y = bboxmin.y; P.y <= bboxmax.y; P.y++)
+		{
+			Vec3f bc_screen = barycentric(pts[0], pts[1], pts[2], P);
+			if (bc_screen.x < 0 || bc_screen.y < 0 || bc_screen.z < 0) continue;
 
+			P.z = 0;
+			for (int i = 0; i < 3; ++i)
+				P.z += pts[i][2] * bc_screen[i];
+
+			if (zbuffer[int(P.x + P.y * width)] < P.z)
+			{
+				zbuffer[int(P.x + P.y * width)] = P.z;
+				image.set(P.x, P.y, color);
+			}
+		}
+	}
+}
+
+Vec3f world2screen(Vec3f v)
+{
+	return Vec3f(int((v.x + 1.) * width / 2. + .5), int((v.y + 1.) * height / 2. + .5), v.z);
 }
 
 int main(int argc, char** argv)
 {
 	TGAImage image(width, height, TGAImage::RGB);
+	float* zbuffer = new float[width * height];
+
 	if (2 == argc)
-	{
 		model = new Model(argv[1]);
-	}
 	else
-	{
 		model = new Model("res/african_head/african_head.obj");
-	}
 
 	Vec3f light_dir(0, 0, -1);
-	for (int i = 0; i < model->nfaces(); i++)
+	for (int i = 0; i < model->nfaces(); ++i)
 	{
 		std::vector<int> face = model->face(i);
-		Vec2i screen_coords[3];
-		Vec3f world_coords[3];
-		for (int j = 0; j < 3; j++)
-		{
-			Vec3f v = model->vert(face[j]);
-			screen_coords[j] = Vec2i((v.x + 1.0) * width / 2.0, (v.y + 1.0) * height / 2.0);
-			world_coords[j] = v;
-		}
-		Vec3f n = (world_coords[2] - world_coords[0]) ^ (world_coords[1] - world_coords[0]);
-		n.normalize();
-		float intensity = n * light_dir;
-		if (intensity > 0)
-		{
-			triangle(screen_coords[0], screen_coords[1], screen_coords[2], image, TGAColor(rand() % 255, rand() % 255, rand() % 255, 255));
-		}
+		Vec3f pts[3];
+		for (int j = 0; j < 3; ++j)
+			pts[j] = world2screen(model->vert(face[j]));
+
+		triangle(pts, zbuffer, image, TGAColor(rand() % 255, rand() % 255, rand() % 255, 255));
 	}
 
 	image.flip_vertically();
 	image.write_tga_file("output.tga");
 	system("output.tga");
-
+	delete model;
 	return 0;
 }
 
