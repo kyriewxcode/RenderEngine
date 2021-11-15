6057e5bca12472c4e63723b48a0d31d312d742ad
Kyrie <61609701+kyriewxcode@users.noreply.github.com>
2021-11-15 15:23:41 +0800

    光栅化

--- main.cpp
+++ main.cpp
@@ -6,82 +6,96 @@
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
+const TGAColor green = TGAColor(0, 255, 0, 255);
 Model* model = NULL;
 const int width = 800;
 const int height = 800;
 
-void line(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color)
+void line(Vec2i p0, Vec2i p1, TGAImage& image, TGAColor color)
 {
 	bool steep = false;
-	if (std::abs(x0 - x1) < std::abs(y0 - y1))
+	if (std::abs(p0.x - p1.x) < std::abs(p0.y - p1.y))
 	{
-		std::swap(x0, y0);
-		std::swap(x1, y1);
+		std::swap(p0.x, p0.y);
+		std::swap(p1.x, p1.y);
 		steep = true;
 	}
-	if (x0 > x1)
-	{
-		std::swap(x0, x1);
-		std::swap(y0, y1);
-	}
-	int dx = x1 - x0;
-	int dy = y1 - y0;
-	int derror2 = std::abs(dy) * 2;
-	int error2 = 0;
-	int y = y0;
 
-	if (steep)
+	if (p0.x > p1.x)
 	{
-		for (int x = x0; x <= x1; x++)
+		std::swap(p0, p1);
+	}
+
+	for (int x = p0.x; x <= p1.x; x++)
+	{
+		float t = (x - p0.x) / (float)(p1.x - p0.x);
+		int y = p0.y * (1.0 - t) + p1.y * t;
+		if (steep)
 		{
 			image.set(y, x, color);
-			error2 += derror2;
-			if (error2 > dx)
-			{
-				y += (y1 > y0 ? 1 : -1);
-				error2 -= dx * 2;
-			}
 		}
-	}
-	else
-	{
-		for (int x = x0; x <= x1; x++)
+		else
 		{
 			image.set(x, y, color);
-			error2 += derror2;
-			if (error2 > dx)
-			{
-				y += (y1 > y0 ? 1 : -1);
-				error2 -= dx * 2;
-			}
 		}
 	}
 }
 
+void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage& image, TGAColor color)
+{
+	if (t0.y == t1.y && t0.y == t2.y) return; // 不关心退化三角形
+	if (t0.y > t1.y) std::swap(t0, t1);
+	if (t0.y > t2.y) std::swap(t0, t2);
+	if (t1.y > t2.y) std::swap(t1, t2);
+
+	int total_height = t2.y - t0.y;
+	for (int i = 0; i < total_height; i++)
+	{
+		bool second_half = i > t1.y - t0.y || t1.y == t0.y;	// 判断是否上半三角形
+		int segment_height = second_half ? t2.y - t1.y : t1.y - t0.y;
+		float alpha = (float)i / total_height;
+		float beta = (float)(i - (second_half ? t1.y - t0.y : 0)) / segment_height; // 不可以除零
+		Vec2i A = t0 + (t2 - t0) * alpha;
+		Vec2i B = second_half ? t1 + (t2 - t1) * beta : t0 + (t1 - t0) * beta;
+		if (A.x > B.x) std::swap(A, B);
+		for (int j = A.x; j < B.x; j++)
+		{
+			image.set(j, t0.y + i, color); // t0.y+i != A.y
+		}
+	}
+
+}
+
 int main(int argc, char** argv)
 {
-	if (argc == 2)
+	TGAImage image(width, height, TGAImage::RGB);
+	if (2 == argc)
 	{
 		model = new Model(argv[1]);
 	}
 	else
 	{
-		model = new Model("res/diablo3_pose/diablo3_pose.obj");
+		model = new Model("res/african_head/african_head.obj");
 	}
 
-	TGAImage image(width, height, TGAImage::RGB);
+	Vec3f light_dir(0, 0, -1);
 	for (int i = 0; i < model->nfaces(); i++)
 	{
 		std::vector<int> face = model->face(i);
+		Vec2i screen_coords[3];
+		Vec3f world_coords[3];
 		for (int j = 0; j < 3; j++)
 		{
-			Vec3f v0 = model->vert(face[j]);
-			Vec3f v1 = model->vert(face[(j + 1) % 3]);
-			int x0 = (v0.x + 1.) * width / 2.;
-			int y0 = (v0.y + 1.) * height / 2.;
-			int x1 = (v1.x + 1.) * width / 2.;
-			int y1 = (v1.y + 1.) * height / 2.;
-			line(x0, y0, x1, y1, image, white);
+			Vec3f v = model->vert(face[j]);
+			screen_coords[j] = Vec2i((v.x + 1.0) * width / 2.0, (v.y + 1.0) * height / 2.0);
+			world_coords[j] = v;
+		}
+		Vec3f n = (world_coords[2] - world_coords[0]) ^ (world_coords[1] - world_coords[0]);
+		n.normalize();
+		float intensity = n * light_dir;
+		if (intensity > 0)
+		{
+			triangle(screen_coords[0], screen_coords[1], screen_coords[2], image, TGAColor(rand() % 255, rand() % 255, rand() % 255, 255));
 		}
 	}
 
@@ -89,7 +103,6 @@ int main(int argc, char** argv)
 	image.write_tga_file("output.tga");
 	system("output.tga");
 
-	delete model;
 	return 0;
 }
 
